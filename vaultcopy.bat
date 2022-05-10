b4c0c690351f49bd47fb81202a41834e0e7d5f40
MulfoK <kallan.mulford@gmail.com>
2022-05-10 19:29:29 +0100

    Working on custom icons

--- vaultcopy.bat
+++ vaultcopy.bat
@@ -1,2 +1,7 @@
+:: Navigates to root folder
 pushd C:\Users\User
-ROBOCOPY "./Desktop/Coding/Repos/obsidian-viridian" "./Documents/mulfok's Vault/.obsidian/themes" Viridian.css /MON:1
+
+:: Copies from repo into vault
+:top
+ROBOCOPY "./Desktop/Coding/Repos/obsidian-viridian" "./Documents/mulfok's Vault/.obsidian/themes" Viridian.css & :: /MON:1 would work, but have to wait a whole minute
+goto :top
