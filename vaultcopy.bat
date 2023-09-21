0fc5a8833ff193594ee38b94c0f0b9da960456d8
MulfoK <kallan.mulford@gmail.com>
2023-09-21 14:24:51 +0100

    Various fixes and changes
    - Blockquotes have padding again
    - Callouts have their original layout, but text is still not in the right spot
    - Don't know what I was doing with codeblocks, so stuff there has been commented out
    - Started tinkering with font weights, but no effect seen really
    - Migrating some Viridian variables to instead use Obsidian ones
    - Got rid of vaultcopy.bat

--- vaultcopy.bat
+++
@@ -1,7 +0,0 @@
-:: Navigates to root folder
-pushd C:\Users\User
-
-:: Copies from repo into vault
-:top
-ROBOCOPY "./Desktop/Coding/Repos/obsidian-viridian" "./Documents/mulfok's Vault/.obsidian/themes" Viridian.css & :: /MON:1 would work, but have to wait a whole minute
-goto :top
